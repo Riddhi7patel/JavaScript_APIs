a1c57d4a7d7e5cd9afe094af01be1ee518915931
Riddhi Patel <Ridshah2@gmail.com>
2023-01-23 12:32:46 -0500

    APIs

--- app.js
+++ app.js
@@ -2,3 +2,26 @@ console.log("Hello World!\n==========\n");
 
 // Exercise 1 Section
 console.log("EXERCISE 1:\n==========\n");
+const searchInput = document.getElementById("searchWord");
+const feedbackEle = document.querySelector("p");
+const searchBtn = document.getElementById("submitSearch");
+const img = document.querySelector("img");
+
+searchBtn.addEventListener("click", () => {
+  fetch(
+    "https://api.giphy.com/v1/gifs/translate?api_key=oFV08QkCTABmY4ClUrQgwswjjx0YROhM&s=" +
+      searchInput.value,
+    { mode: "cors" }
+  )
+    .then((response) => response.json())
+    .then((res) => {
+      feedbackEle.textContent = "";
+      img.src = res.data.images.original.url;
+      searchInput.value = "";
+    })
+    .catch((err) => {
+      console.error(err);
+      feedbackEle.textContent = err.message;
+    });
+});
+
